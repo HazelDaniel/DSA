5ac3293eab4bd4cc2d4be4c03b88a176ba4798da
HazelDaniel <hazeldaniel856@gmail.com>
2023-02-06 13:58:10 -0600

    # 'finding number of inversions in an array' solution (issue: not working yet)

--- finding-inversions.cc
+++ finding-inversions.cc
@@ -9,8 +9,8 @@ using std::boolalpha;
 using std::floor;
 using std::vector;
 
-void merge_sort(vector<int> &list, size_t s_index, size_t l_index, size_t &inversion);
-void merge(vector<int> &list, size_t s_index, size_t middle, size_t l_index, size_t &inversion);
+size_t merge_sort(vector<int> &list, size_t s_index, size_t l_index);
+size_t merge(vector<int> &list, size_t s_index, size_t middle, size_t l_index);
 
 
 int main (){
@@ -18,7 +18,6 @@ int main (){
 
     vector<int> arr_list {1,4,0,6,2,8};
 
-    size_t inversion {0};
 
     cout << "list before sorting : ";
 
@@ -26,84 +25,84 @@ int main (){
         cout << item << " " ;
     }
 
-    cout << "\n list after sorting : ";
-    merge_sort(arr_list,0,arr_list.size() - 1,inversion);
+    cout << endl;
+
+    cout << merge_sort(arr_list,0,arr_list.size() - 1) << " inversions found in the array" << endl;
+    cout << "list after sorting : ";
 
     for(auto const &item: arr_list){
         cout << item << " ";
     }
     
     cout << "" << endl;
-    cout << "number of inversions : " << inversion << endl;
 
     return 0;
 
 }
 
-void merge(vector<int> &list, size_t s_index, size_t middle, size_t l_index, size_t &inversion){
+size_t merge(vector<int> &list, size_t s_index, size_t middle, size_t l_index){
 
-    size_t n1{middle - s_index + 1};
-    size_t n2{l_index - middle};
+    size_t inv{0};
+    size_t n1 {middle - s_index + 1};
+    size_t n2 {l_index - middle};
 
     vector<int> L (n1+1);
     vector<int> R (n2+1);
 
-
-    for(size_t i {0}; i < n1; i ++){
+    for(size_t i {0}; i < n1; i++){
         L.at(i) = list.at(s_index + i);
+    };
+    for(size_t j {0}; j < n2; j++){
+        R.at(j) = list.at(middle+j);
     }
-
-    for(size_t j {0}; j < n2; j ++){
-        R.at(j) = list.at(middle + j + 1);
-    }
-
-    L.at(n1) = static_cast<int>(MAXFLOAT);
-    R.at(n2) = static_cast<int>(MAXFLOAT);
 
     size_t i {0};
     size_t j {0};
+    size_t k {s_index};
 
-    size_t inv1 {0};
-    size_t inv2 {0};
-
-    for(size_t k {s_index}; k <= l_index; k++){
-
+    while(i != n1 && j != n2){
         if(L.at(i) <= R.at(j)){
             list.at(k) = L.at(i);
-
-            if(k != i ){
-                inv1++;
-            }
-
             i++;
         }else{
             list.at(k) = R.at(j);
-
-            if(k != j + n1 ){
-                inv2++;
-            }
-
+            inv+=j;
             j++;
         }
-
     }
-
-    if(!(static_cast<int>(inv1 + inv2 - 1) < 0)){
-        inversion += (inv1 + inv2 - 1);
+    if(i == n1){
+        for(size_t m {j}; m < n2; m++){
+            list.at(k) = R.at(m);
+            k++;
+        }
     }
-    cout << inversion << " inversions here " << endl;
+    if(j == n2){
+        for(size_t m {i}; m < n1; m++){
+            list.at(k) = L.at(m);
+            inv+=n2;
+            k++;
+        }
+    }
+    return inv;
+
 
 }
 
-void merge_sort(vector<int> &list, size_t s_index, size_t l_index, size_t &inversion){
+size_t merge_sort(vector<int> &list, size_t s_index, size_t l_index){
     
-   if(s_index < l_index){
+    size_t inv {0};
+
+    if(s_index < l_index){
 
         size_t middle {static_cast<size_t>(floor((s_index + l_index)/2))};
+        size_t left {0};
+        size_t right {0};
 
-        merge_sort(list, s_index, middle, inversion);
-        merge_sort(list, middle + 1, l_index, inversion);
-        merge(list,s_index,middle,l_index, inversion);
-   }
+
+        left = merge_sort(list, s_index, middle);
+        right = merge_sort(list, middle + 1, l_index);
+        inv = merge(list,s_index,middle,l_index) + left + right;
+    }
+    return inv;
 
 }
