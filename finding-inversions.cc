93b528b2fd784d259f1c86a3dadd5c9e8cae92f0
HazelDaniel <hazeldaniel856@gmail.com>
2023-02-07 06:08:24 -0600

    # 'finding number of inversions in an array' works (#issue not for duplicate numbers)

--- finding-inversions.cc
+++ finding-inversions.cc
@@ -16,7 +16,7 @@ size_t merge(vector<int> &list, size_t s_index, size_t middle, size_t l_index);
 int main (){
     cout << boolalpha;
 
-    vector<int> arr_list {1,4,0,6,2,8};
+    vector<int> arr_list {2,6,7,8,0,5,5,4};
 
 
     cout << "list before sorting : ";
@@ -42,49 +42,42 @@ int main (){
 
 size_t merge(vector<int> &list, size_t s_index, size_t middle, size_t l_index){
 
-    size_t inv{0};
-    size_t n1 {middle - s_index + 1};
-    size_t n2 {l_index - middle};
+    size_t n1{middle - s_index + 1};
+    size_t n2{l_index - middle};
+    size_t inv {0};
 
     vector<int> L (n1+1);
     vector<int> R (n2+1);
 
-    for(size_t i {0}; i < n1; i++){
+
+    for(size_t i {0}; i < n1; i ++){
         L.at(i) = list.at(s_index + i);
-    };
-    for(size_t j {0}; j < n2; j++){
-        R.at(j) = list.at(middle+j);
     }
+
+    for(size_t j {0}; j < n2; j ++){
+        R.at(j) = list.at(middle + j + 1);
+    }
+
+    L.at(n1) = static_cast<int>(MAXFLOAT);
+    R.at(n2) = static_cast<int>(MAXFLOAT);
 
     size_t i {0};
     size_t j {0};
-    size_t k {s_index};
 
-    while(i != n1 && j != n2){
+    for(size_t k {s_index}; k <= l_index; k++){
+
         if(L.at(i) <= R.at(j)){
             list.at(k) = L.at(i);
             i++;
-        }else{
+        }
+        else{
+            inv = middle - i;
             list.at(k) = R.at(j);
-            inv+=j;
             j++;
         }
-    }
-    if(i == n1){
-        for(size_t m {j}; m < n2; m++){
-            list.at(k) = R.at(m);
-            k++;
-        }
-    }
-    if(j == n2){
-        for(size_t m {i}; m < n1; m++){
-            list.at(k) = L.at(m);
-            inv+=n2;
-            k++;
-        }
+
     }
     return inv;
-
 
 }
 
@@ -95,13 +88,10 @@ size_t merge_sort(vector<int> &list, size_t s_index, size_t l_index){
     if(s_index < l_index){
 
         size_t middle {static_cast<size_t>(floor((s_index + l_index)/2))};
-        size_t left {0};
-        size_t right {0};
 
-
-        left = merge_sort(list, s_index, middle);
-        right = merge_sort(list, middle + 1, l_index);
-        inv = merge(list,s_index,middle,l_index) + left + right;
+        inv = merge_sort(list, s_index, middle);
+        inv += merge_sort(list, middle + 1, l_index);
+        inv += merge(list,s_index,middle,l_index);
     }
     return inv;
 
