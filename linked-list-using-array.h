9eababb3f2fa22d54256a06d3514e03a4f9998f8
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-25 05:15:17 +0100

    initialized header file for KENPHF list implementation

---
+++ linked-list-using-array.h
@@ -0,0 +1,9 @@
+#ifndef ___LINKED_LIST_USING_ARRAY_
+#define ___LINKED_LIST_USING_ARRAY_
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+#define OOB (-1)
+#endif/*___LINKED_LIST_USING_ARRAY_*/
