674b8240a07963c781d10b698144a78252bb527f
HazelDaniel <hazeldaniel856@gmail.com>
2024-01-10 06:35:12 +0100

    O(n) solution to leetcode 23

--- leetcode-23.py
+++ leetcode-23.py
@@ -13,20 +13,6 @@ class Solution:
         glob_list = []
         diff_hash = defaultdict(list)
 
-        def str_(self):
-            return f"{self.val}"
-
-        def length_(self):
-            count = 0
-            curr = self
-            while curr:
-                curr = curr.next
-                count += 1
-            return count
-        ListNode.__str__ = str_
-        ListNode.__repr__ = str_
-        ListNode.length = length_
-
         for i in lists:
             curr = i
             while curr:
@@ -41,17 +27,17 @@ class Solution:
             diff = l_max - i.val
             if diff_hash[diff]:
                 diff_hash[diff][-1].next = i
+                diff_hash[diff].append(i)
             else:
                 diff_hash[diff].append(i)
         i = 0
         prev = None
         prev_arr = []
         head = diff_hash[l_max - l_min][0]
-        # print(f"diff hash is {diff_hash}")
         while i <= l_max - l_min:
             prev_arr = diff_hash[i]
-            prev_arr[-1].next = prev
-            prev = prev_arr[0]
-            # print(f"prev value is {prev.val}")
+            if (prev_arr):
+                prev_arr[-1].next = prev
+                prev = prev_arr[0]
             i += 1
         return head
